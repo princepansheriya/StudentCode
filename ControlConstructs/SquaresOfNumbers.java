3a01c2f7fcb4db9867c41076349530746356daa6
princepansheriya <141042319+princepansheriya@users.noreply.github.com>
2023-08-22 12:46:07 +0530

    Add files via upload

---
+++ ControlConstructs/SquaresOfNumbers.java
@@ -0,0 +1,31 @@
+package com.kn.forloop;
+
+import java.util.Scanner;
+
+//Write a program to print the sum of squares of n numbers.
+public class SquaresOfNumbers {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter base");
+		int base=s.nextInt();
+		System.out.println("Enter exponent");
+		int exponent=s.nextInt();
+		
+		System.out.println(name(base,exponent));
+
+	}
+	
+	public static int name(int a,int b) {
+		int result=1;
+		
+		for(int i=1;i<=b;i++) {
+			
+			result=result*a;
+		}
+		return result;
+		
+		
+	}
+
+}
