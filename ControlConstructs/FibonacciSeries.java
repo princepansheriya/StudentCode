bf9492faf6bff3a66ce25a78c81700592f4a4bf7
princepansheriya <141042319+princepansheriya@users.noreply.github.com>
2023-08-22 12:35:36 +0530

    Add files via upload

--- ControlConstructs/FibonacciSeries.java
+++ ControlConstructs/FibonacciSeries.java
@@ -2,31 +2,27 @@ package com.kn.forloop;
 
 import java.util.Scanner;
 
-//Write a program to print the Fibonacci series up to n terms.
 public class FibonacciSeries {
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		System.out.println("Enter number");
 		
-		int num=s.nextInt();
+		int num = s.nextInt();
 
 		fibonacci(num);
-
 	}
 	
 	static void fibonacci(int num) {
-		int firstNumber = 0, secondNumber = 1, thierdNumber = 0;
-		System.out.print(firstNumber);
+		int firstNumber = 0, secondNumber = 1, thirdNumber = 0;
+		System.out.print(firstNumber + " " + secondNumber);
 
-		for (int i = 3; i <= num; i++) {
+		for (int i = 2; i < num; i++) {
+			thirdNumber = firstNumber + secondNumber;
+			System.out.print(" " + thirdNumber);
+
 			firstNumber = secondNumber;
-			secondNumber = thierdNumber;
-			thierdNumber = firstNumber + secondNumber;
-			System.err.print(" "+thierdNumber);
-
+			secondNumber = thirdNumber;
 		}
-		
 	}
-
 }
